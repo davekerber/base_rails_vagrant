e3203ddac808e093c80a901e1204ba9ee5afa1d2
David Kerber <dave@agapered.com>
2013-05-03 13:54:44 -0500

    dont try to copy the file if it does not exist

--- install_custom_setup.sh
+++ install_custom_setup.sh
@@ -1,3 +1,7 @@
-cp /host_folder/custom_vagrant_setup.sh /etc/profile.d/custom_vagrant_setup.sh
-chown root:root /etc/profile.d/custom_vagrant_setup.sh
-chmod 755 /etc/profile.d/custom_vagrant_setup.sh
+custom_setup_script="/host_folder/custom_vagrant_setup.sh"
+if [ -f $custom_setup_script ];
+then	
+	cp $custom_setup_script /etc/profile.d/custom_vagrant_setup.sh
+	chown root:root /etc/profile.d/custom_vagrant_setup.sh
+	chmod 755 /etc/profile.d/custom_vagrant_setup.sh
+fi
