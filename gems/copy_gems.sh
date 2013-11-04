4bd57bc3b47b13217c240b92e150e138bf05eb44
Alec Larsen <alec.larsen@agapered.com>
2013-11-04 17:14:35 -0600

    Revert "udpating for new requirements"
    This reverts commit 99cb455ffd3cd04398a4aedc69ebeef2db04749c.

--- gems/copy_gems.sh
+++
@@ -1,2 +0,0 @@
-sudo cp    /vagrant/gems/specifications/* /opt/vagrant_ruby/lib/ruby/gems/1.8/specifications/
-sudo cp -R /vagrant/gems/gems/*           /opt/vagrant_ruby/lib/ruby/gems/1.8/gems/
--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,13 @@
+include rvm
+include agapered_pg
 include agapered_git
 include agapered_nodocs
 
-package { ['vim','zip','imagemagick','mysql-server-5.5', 'mysql-client-5.5', 'libmysqlclient-dev', 'libmagickcore-dev', 'libmagickwand-dev'] :
+package { ['vim','zip','imagemagick'] :
   ensure => present
 }
+
+rvm::system_user { vagrant: ; }
+
+
 
